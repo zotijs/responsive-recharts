d81f2612919aa3f178e1a11b016aef235e80b71a
zotijs <33979528+zotijs@users.noreply.github.com>
2020-05-14 12:29:01 +0300

    changing index

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
+// playing around with git
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
