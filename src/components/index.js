2ac0208c09e4ad002ee7b38b9ff3835a8b062ca6
zotijs <33979528+zotijs@users.noreply.github.com>
2020-05-14 12:29:41 +0300

    components index change

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import { FlexContainer, GridContainer } from "components/containers";
 import { SeasonalityChart } from "components/graphs";
 import SizeCalculator from "components/sizeCalculator";
-
+// playing around with git
 export { FlexContainer, GridContainer, SeasonalityChart, SizeCalculator };
